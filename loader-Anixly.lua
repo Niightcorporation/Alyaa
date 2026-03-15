35947daff9ba81a7b403f8529fdc35de6a28cdc9
Niightcorporation <dikakimasadepan@gmail.com>
2026-03-16 03:45:10 +0700

    Add files via upload

--- loader-Anixly.lua
+++ loader-Anixly.lua
@@ -5,7 +5,7 @@ local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 local Games = {
-    [131623223084840] = {
+    [130342654546662] = {
         name = "sambung-kata",
         url = "https://raw.githubusercontent.com/Niightcorporation/Alyaa/refs/heads/main/Anixly.lua"
     }
