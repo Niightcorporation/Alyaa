218937298b6ba16849fa2c75d24cf2a721fe36c4
Niightcorporation <dikakimasadepan@gmail.com>
2026-03-16 04:01:11 +0700

    Add files via upload

--- loader/loader-Anixly.lua
+++ loader/loader-Anixly.lua
@@ -1,1 +1,27 @@
+do
+repeat task.wait() until game:IsLoaded()
 
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local Games = {
+    [130342654546662] = {
+        name = "sambung-kata",
+        url = "https://raw.githubusercontent.com/Niightcorporation/Alyaa/refs/heads/main/Anixly.lua"
+    }
+}
+
+local placeId = game.PlaceId
+local gameData = Games[placeId]
+
+if gameData then
+    loadstring(game:HttpGet(gameData.url))()
+else
+    local supported = ""
+    for id,data in pairs(Games) do
+        supported = supported .. "\n• " .. data.name .. " (ID: " .. id .. ")"
+    end
+
+    player:Kick("Game not supported!\n\nSupported Games:" .. supported .. "\n\nCurrent Place ID: " .. placeId)
+end
+end
